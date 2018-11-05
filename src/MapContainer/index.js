7ee03ee341b748ca440c2431b9dae7ad36cc78d1
John <john.hardberger@gmail.com>
2018-11-05 17:10:44 -0600

    long day of attempting to create polygons in the google maps react api - no luck yet but commit pre: trying something weird

--- src/MapContainer/index.js
+++ src/MapContainer/index.js
@@ -1,46 +1,44 @@
 import React, { Component } from 'react';
+import { Map, maps, GoogleApiWrapper, google } from 'google-maps-react';
 
+import Key from '../api_key';
 
-class MapContainer extends Component {
-	render(){
-		return(
-			<body>
-			    <div id="map"></div>
-			    <script>
-			      function initMap() {
+export class MapContainer extends Component {
+  constructor(){
+    super();
+    this.state = {
+      
+    }
+  }
+  itsAlive = () => {
+    console.log(`it's alive!`);
+  }
+  loadMapShapes = () => {
+      console.log(`this works!`);
+      maps.data.loadGeoJson('https://storage.googleapis.com/mapsdevsite/json/states.js', 
+        { idPropertyName: 'STATE' }
+      );
 
-			        // load the map
-			        map = new google.maps.Map(document.getElementById('map'), {
-			          center: {lat: 40, lng: -100},
-			          zoom: 4,
-			          styles: mapStyle
-			        });
+  }
+  componentDidMount(){
+    this.loadMapShapes();
+  }
+  render() {
+    return (
 
-			        var mapStyle = [{
-			          'featureType': 'all',
-			          'elementType': 'all',
-			          'stylers': [{'visibility': 'off'}]
-			        }, {
-			          'featureType': 'landscape',
-			          'elementType': 'geometry',
-			          'stylers': [{'visibility': 'on'}, {'color': '#fcfcfc'}]
-			        }, {
-			          'featureType': 'water',
-			          'elementType': 'labels',
-			          'stylers': [{'visibility': 'off'}]
-			        }, {
-			          'featureType': 'water',
-			          'elementType': 'geometry',
-			          'stylers': [{'visibility': 'on'}, {'hue': '#5f94ff'}, {'lightness': 60}]
-			        }];
-			      }
+        <Map google={this.props.google} 
+          initialCenter={{
+            lat: 41.878, 
+            lng: -87.629
+          }}
+          zoom={14}
+        >
+        </Map>
 
-			    </script>
-			    <script async defer
-			        src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&callback=initMap">
-			    </script>
-  			</body>
-		)
-	}
+    );
+  }
 }
-export default MapContainer;
+ 
+export default GoogleApiWrapper({
+  apiKey: (Key)
+})(MapContainer)
