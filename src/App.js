f6621ccbd533a60f0c926fcae2cdbef3cb3e82ee
LehmanBrother <chrislehman1991@gmail.com>
2018-11-05 15:19:45 -0600

    Made initial StateContainer

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import apiKey from './apiKey.js'
+import StateContainer from './StateContainer';
+
+const baseEndPoint = 'https://api.census.gov/data/2017/pep/population?get='
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+
+    }
+  }
+
+  //gets all states + relevant info for intiial seed
+  getAllStates = async () => {
+    try {
+      const allStates = await fetch(baseEndPoint + 'GEONAME,POP,DENSITY&for=state');
+      console.log(allStates);
+      const allStatesJson = await allStates.json();
+      console.log(allStatesJson);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  componentDidMount(){
+    console.log('cdm');
+    //this.getAllStates();
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <StateContainer />
       </div>
     );
   }
---
+++ src/StateContainer/index.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+class StateContainer extends Component {
+	constructor(){
+		super();
+		this.state = {
+
+		}
+	}
+	render(){
+		return (
+			<p>StateContainer</p>
+		)
+	}
+}
+
+export default StateContainer
