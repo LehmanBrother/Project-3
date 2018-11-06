ce43d06c87f8b6044b17aae57888b42449c162d5
LehmanBrother <chrislehman1991@gmail.com>
2018-11-06 10:54:47 -0600

    Improved state search function

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import apiKey from './apiKey.js'
 import StateContainer from './StateContainer';
+import SearchContainer from './SearchContainer';
 
 const baseEndPoint = 'https://api.census.gov/data/2017/pep/population?get='
 
@@ -10,7 +11,12 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-
+      currentState: {
+        name: '',
+        pop: 0,
+        density: 0,
+        code: 0
+      }
     }
   }
 
@@ -26,6 +32,28 @@ class App extends Component {
     }
   }
 
+  geoSearch = async (search, e) => {
+    e.preventDefault();
+    try {
+      const searchedGeo = await fetch('http://localhost:9000/api/v1/census/stateSearch/' + search.searchText);
+      const parsedResponse = await searchedGeo.json();
+      const censusState = await fetch(baseEndPoint + 'GEONAME,POP,DENSITY&for=state:' + parsedResponse.data[0].code)
+      const censusStateJson = await censusState.json();
+      this.setState({
+        //censusStateJson is an array that contains one dummy entry and one or more state entries, so we use [1]
+        currentState: {
+          name: censusStateJson[1][0],
+          pop: censusStateJson[1][1],
+          density: censusStateJson[1][2],
+          code: censusStateJson[1][3]
+        }
+      })
+      console.log(this.state);
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
   componentDidMount(){
     console.log('cdm');
     //this.getAllStates();
@@ -35,6 +63,7 @@ class App extends Component {
     return (
       <div className="App">
         <StateContainer />
+        <SearchContainer geoSearch={this.geoSearch} />
       </div>
     );
   }
---
+++ src/SearchContainer/index.js
@@ -0,0 +1,40 @@
+import React, {Component} from 'react';
+
+class SearchContainer extends Component {
+	constructor(){
+		super();
+		this.state = {
+			searchText: '',
+			searchType: 'State'
+		}
+	}
+	updateSearch = (e) => {
+		this.setState({
+			[e.currentTarget.name]: e.currentTarget.value
+		});
+	}
+	render(){
+		console.log(this.state);
+		return (
+			<div>
+				<p>SearchContainer</p>
+				<form onSubmit={this.props.geoSearch.bind(null, this.state)}>
+					<label>
+						Search Type: 
+						<select name='searchType' value={this.state.searchType} onChange={this.updateSearch}>
+							<option>State</option>
+							<option>City</option>
+						</select>
+					</label><br/>
+					<label>
+						Search: 
+						<input type='text' name='searchText' value={this.state.searchText} onChange={this.updateSearch}/>
+					</label><br/>
+					<button type='Submit'>Search</button>
+				</form>
+			</div>
+		)
+	}
+}
+
+export default SearchContainer
