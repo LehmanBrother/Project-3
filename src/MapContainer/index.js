fc1f7a0bdd2f4b9eb681de3655db074f751bf743
John <john.hardberger@gmail.com>
2018-11-06 11:54:25 -0600

    trying to hard-code in map data with little luck

--- src/MapContainer/index.js
+++ src/MapContainer/index.js
@@ -1,27 +1,30 @@
 import React, { Component } from 'react';
 import {Map, Polygon, GoogleApiWrapper} from 'google-maps-react';
+import coordinates from '../Coordinates';
 import Key from '../api_key'; 
 
 export class MapContainer extends Component {
+  constructor(){
+    super();
+    this.state = {}
+  }
+  componentDidMount(){
+    console.log(coordinates.features[0].geometry.coordinates[0]);
+  }
   render() {
-    const triangleCoords = [
-      {lat: 25.774, lng: -80.190},
-      {lat: 18.466, lng: -66.118},
-      {lat: 32.321, lng: -64.757},
-      {lat: 25.774, lng: -80.190}
-    ];
     return (
       <Map 
         google={this.props.google} 
         zoom={14}>
 
         <Polygon
-          paths={triangleCoords}
+          path={coordinates.features[0].geometry.coordinates[0]}
           strokeColor="#0000FF"
           strokeOpacity={0.8}
           strokeWeight={2}
           fillColor="#0000FF"
           fillOpacity={0.35} />
+    
 
       </Map>
     );
